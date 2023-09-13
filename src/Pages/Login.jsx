f0ceb0e123ac0f75c8fad3f47435d5b8a0c02231
Jack Ball <ball.jack2376@gmail.com>
2023-09-13 16:32:19 -0600

    working on login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,26 @@
-import React from "react";
 import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-  const click = async (req, res) => {
-    await axios.get("/app/addUser");
-    console.log(res);
+  const [username, setUsername] = useState("username");
+  const [password, setPassword] = useState("password");
+  const [fname, setFName] = useState("first name");
+  const [lname, setLName] = useState("last name");
+  const [register, setRegister] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    axios.post(register ? "/api/register" : "/api/login", {
+      username,
+      password,
+      fname,
+      lname,
+    });
   };
 
   return (
     <>
-      <h1>Login</h1>
+      <h1>Login Below</h1>
       <button onClick={click}>create user</button>
     </>
   );
